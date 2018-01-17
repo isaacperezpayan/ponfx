c4df0ebb850d32c00c9182e4db20322d64348d8e
isaac <isaac@DESKTOP-G6SCT5K>
2018-01-17 10:16:04 +0100

    Creando la pala y su movimiento

--- src/ponfxbueno/Ponfxbueno.java
+++ src/ponfxbueno/Ponfxbueno.java
@@ -7,10 +7,14 @@ package ponfxbueno;
 /*Importamos las distintas clases para su posterior utilizacion en objetos*/
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane; /*Layout para colocacion de elementos en ventana*/
 import javafx.scene.paint.Color; /*paint.Color para obtener los distintos colores*/
 import javafx.scene.shape.Circle; /*Importamos Clase geometrica (Circulo)*/
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -22,10 +26,17 @@ public class Ponfxbueno extends Application {
     short bolaCenterY=30;
     byte velocidadbolay=3;
     byte velocidadbolax=3;
+    final int SCENE_TAM_X= 600;
+    final int SCENE_TAM_Y= 400;
+    final int STICK_WIDTH= 7;
+    final int STICK_HEIGHT= 50;
+    int stickPosy = (SCENE_TAM_Y - STICK_HEIGHT) / 2;
+    int stickCurrentSpeed = 0;
+    
     @Override
     public void start(Stage primaryStage) {
         Pane root= new Pane();
-        Scene scene = new Scene (root, 600, 400);
+        Scene scene = new Scene (root, SCENE_TAM_X, SCENE_TAM_Y);
         scene.setFill(Color.YELLOW);
         primaryStage.setTitle("PongFX");
         primaryStage.setScene(scene);
@@ -35,11 +46,13 @@ public class Ponfxbueno extends Application {
         bola.setCenterY(30);
         bola.setRadius(7);
         AnimationTimer movimiento;
+        Rectangle rectStick = new Rectangle (SCENE_TAM_X*0.9, stickPosy, STICK_WIDTH, STICK_HEIGHT);
+        rectStick.setFill(Color.GREEN);
         movimiento = new AnimationTimer() {
             public void handle(long now) {
                 bola.setCenterX(bolaCenterX);
                 bolaCenterX+= velocidadbolax;
-                if (bolaCenterX >= 600){
+                if (bolaCenterX >= SCENE_TAM_X){
                     velocidadbolax = -3;
                 }
                 if (bolaCenterX <= 0){
@@ -47,7 +60,7 @@ public class Ponfxbueno extends Application {
                 }
                 bola.setCenterY(bolaCenterY);
                 bolaCenterY+= velocidadbolay;
-                if (bolaCenterY >= 400){
+                if (bolaCenterY >= SCENE_TAM_Y){
                     velocidadbolay = -3;
                 }
                 if (bolaCenterY <= 0){
@@ -55,7 +68,26 @@ public class Ponfxbueno extends Application {
                 }
             };
         };
+        scene.setOnKeyReleased((KeyEvent event) -> {
+            switch(event.getCode()){
+                case UP:
+                    //Pulsada tecla arriba
+                    stickCurrentSpeed = -6;
+                    break;
+                case DOWN:
+                    //Pulsada tecla abajo
+                    stickCurrentSpeed = 6;
+                    break;
+            }
+        });
+        
+        scene.setOnKeyReleased((KeyEvent event) -> {
+            stickCurrentSpeed= 0;
+             
+        });
+        
         root.getChildren().add(bola);
+        root.getChildren().add(rectStick);
         bola.setFill(Color.PURPLE);
         movimiento.start();
         /*Codigo anterior simplificado de las disntintas clases y objetos*/
