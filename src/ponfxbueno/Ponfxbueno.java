75636a156620ddb03533eb1c2d3e1b32dc40774f
isaac <isaac@DESKTOP-G6SCT5K>
2018-01-19 14:34:14 +0100

    ponfx en bruto

--- src/ponfxbueno/Ponfxbueno.java
+++ src/ponfxbueno/Ponfxbueno.java
@@ -7,14 +7,18 @@ package ponfxbueno;
 /*Importamos las distintas clases para su posterior utilizacion en objetos*/
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane; /*Layout para colocacion de elementos en ventana*/
 import javafx.scene.paint.Color; /*paint.Color para obtener los distintos colores*/
 import javafx.scene.shape.Circle; /*Importamos Clase geometrica (Circulo)*/
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -32,11 +36,15 @@ public class Ponfxbueno extends Application {
     final int STICK_HEIGHT= 50;
     int stickPosy = (SCENE_TAM_Y - STICK_HEIGHT) / 2;
     int stickCurrentSpeed = 0;
-    
+    int stickCurrentSpeed2 = 0;
+    short TEXT_SIZE= 24;
+    int score;
+    int highScore;
     
     @Override
     public void start(Stage primaryStage) {
-        
+        //LAYOUTS PARA MOSTRAR PUNTUACIONES
+        //Layout principal
         Pane root= new Pane();
         Scene scene = new Scene (root, SCENE_TAM_X, SCENE_TAM_Y);
         scene.setFill(Color.YELLOW);
@@ -49,7 +57,45 @@ public class Ponfxbueno extends Application {
         bola.setRadius(7);
         AnimationTimer movimiento;
         Rectangle rectStick = new Rectangle (SCENE_TAM_X*0.9, stickPosy, STICK_WIDTH, STICK_HEIGHT);
+        Rectangle rectStick2 = new Rectangle (SCENE_TAM_X*0.1, stickPosy, STICK_WIDTH, STICK_HEIGHT);
         rectStick.setFill(Color.GREEN);
+        //LAYOUTS PARA MOSTRAR PUNTUACIONES
+        //Layout principal
+        HBox paneScores = new HBox();
+        paneScores.setTranslateY(20);
+        paneScores.setMinWidth(SCENE_TAM_X);
+        paneScores.setAlignment(Pos.CENTER);
+        paneScores.setSpacing(100);
+        root.getChildren().add(paneScores);
+        // Layout para puntuacion actual
+        HBox paneCurrentScore = new HBox();
+        paneCurrentScore.setSpacing(10);
+        paneScores.getChildren().add(paneCurrentScore);
+        // Layout para puntuacion maxima
+        HBox paneHighScore = new HBox();
+        paneHighScore.setSpacing(10);
+        paneScores.getChildren().add(paneHighScore);
+        //Texto de etiqueta para la puntuacion
+        Text textTitleScore = new Text("Score: ");
+        textTitleScore.setFont(Font.font(TEXT_SIZE));
+        textTitleScore.setFill(Color.GREY);
+        //Texto para la puntuacion
+        Text textScore = new Text("0");
+        textScore.setFont(Font.font(TEXT_SIZE));
+        textScore.setFill(Color.GRAY);
+        //Texto de la etiqueta para la puntuacion maxima
+        Text textTitleHighScore = new Text("Max.Score: ");
+        textTitleHighScore.setFont(Font.font(TEXT_SIZE));
+        textScore.setFill(Color.GRAY);
+        //Texto para la puntuacion maxima
+        Text textHighScore = new Text("0");
+        textHighScore.setFont(Font.font(TEXT_SIZE));
+        textHighScore.setFill(Color.GRAY);
+        //Añadir los textos a los layouts reservados para ellos
+        paneCurrentScore.getChildren().add(textTitleScore);
+        paneCurrentScore.getChildren().add(textScore);
+        paneHighScore.getChildren().add(textTitleHighScore);
+        paneHighScore.getChildren().add(textHighScore);
         //Creacion de la red del campo
         for(int i=0; i<SCENE_TAM_Y; i+=30){
         Line line = new Line(SCENE_TAM_X/2, i, SCENE_TAM_X/2, i+10);
@@ -61,6 +107,7 @@ public class Ponfxbueno extends Application {
         movimiento = new AnimationTimer() {
             //Actualizar la posicion de la pala
                 
+            @Override
             public void handle(long now) {
                 stickPosy += stickCurrentSpeed;
                 if(stickPosy < 0) {
@@ -71,12 +118,24 @@ public class Ponfxbueno extends Application {
                         stickPosy = SCENE_TAM_Y - STICK_HEIGHT; 
                     }
                   }
-                //Mover rectangulo de la pala a la posicion actual
+         
+                  
+                //Mover rectangulo de la pala derecha a la posicion actual
                 rectStick.setY(stickPosy);
+                rectStick2.setY(stickPosy);
                 bola.setCenterX(bolaCenterX);
+                stickPosy += stickCurrentSpeed;
                 bolaCenterX+= velocidadbolax;
                 if (bolaCenterX >= SCENE_TAM_X){
-                    velocidadbolax = -3;
+                    if (score > highScore) {
+                        highScore = score;
+                        textHighScore.setText(String.valueOf(highScore));
+                    }
+                    //Reiniciar partida
+                    score = 0;
+                    textScore.setText(String.valueOf(score));
+                    bolaCenterX = 10;
+                    velocidadbolay = 3;
                 }
                 if (bolaCenterX <= 0){
                     velocidadbolax = 3;
@@ -90,6 +149,7 @@ public class Ponfxbueno extends Application {
                     velocidadbolay = 3;
                 }
                 
+              
                 scene.setOnKeyPressed((KeyEvent event) -> {
                 switch(event.getCode()){
                 case UP:
@@ -102,39 +162,57 @@ public class Ponfxbueno extends Application {
                     break;
             }
         });
+
+                //Colision barra verde
                 Shape.intersect(bola, rectStick);
+               
+                
                 Shape shapeColision = Shape.intersect(bola, rectStick);
+                
                 boolean colisionVacia = shapeColision.getBoundsInLocal().isEmpty();
-                if(colisionVacia == false){
+               
+                if(colisionVacia == false && velocidadbolax > 0){
                     //Colision detectada. Mover bola hacia la izquierda
                     velocidadbolax = -3;
+                    score++;
+                    textScore.setText(String.valueOf(score));
                 }
+                
                 scene.setOnKeyReleased((KeyEvent event) -> {
                     stickCurrentSpeed= 0;
-             
                 });
-            };
+            
+        
+            
+             
+            }
         };
+
+    
         
-        
+            
         root.getChildren().add(bola);
         root.getChildren().add(rectStick);
+       // root.getChildren().add(rectStick2);
         bola.setFill(Color.PURPLE);
         movimiento.start();
+
         
         /*Codigo anterior simplificado de las disntintas clases y objetos*/
         /*Scene scene = new Scene(root, 600, 400, Color.YELLOW);
         Circle bola = new Circle(10, 30, 7, Color.PURPLE);
         En estas dos lineas hemos declarado obtejos y sus atributos en una sola linea
         */
-        
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    }  
+               
+     public static void main(String[] args) {
         launch(args);
     }
     
 }
+
+ 
+    /**
+     * @param args the command line arguments
+     */
+   
