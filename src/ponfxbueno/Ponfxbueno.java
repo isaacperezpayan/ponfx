944364e1dc208525926714685110f8a28c4e4a19
isaac <isaac@DESKTOP-G6SCT5K>
2018-01-24 11:15:12 +0100

    ponffx terminado 1

--- src/ponfxbueno/Ponfxbueno.java
+++ src/ponfxbueno/Ponfxbueno.java
@@ -5,6 +5,7 @@
  */
 package ponfxbueno;
 /*Importamos las distintas clases para su posterior utilizacion en objetos*/
+import java.util.Random;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Pos;
@@ -28,8 +29,8 @@ import javafx.stage.Stage;
 public class Ponfxbueno extends Application {
     short bolaCenterX=10;
     short bolaCenterY=30;
-    byte velocidadbolay=3;
-    byte velocidadbolax=3;
+    byte velocidadBolay=3;
+    byte velocidadBolax=3;
     final int SCENE_TAM_X= 600;
     final int SCENE_TAM_Y= 400;
     final int STICK_WIDTH= 7;
@@ -40,17 +41,21 @@ public class Ponfxbueno extends Application {
     short TEXT_SIZE= 24;
     int score;
     int highScore;
+    Text textScore;
+    Pane root;
+    
     
     @Override
     public void start(Stage primaryStage) {
         //LAYOUTS PARA MOSTRAR PUNTUACIONES
         //Layout principal
-        Pane root= new Pane();
+        root= new Pane();
         Scene scene = new Scene (root, SCENE_TAM_X, SCENE_TAM_Y);
         scene.setFill(Color.YELLOW);
         primaryStage.setTitle("PongFX");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
         Circle bola = new Circle(/*10, 30, 7*/);
         bola.setCenterX(bolaCenterX);
         bola.setCenterY(30);
@@ -80,7 +85,7 @@ public class Ponfxbueno extends Application {
         textTitleScore.setFont(Font.font(TEXT_SIZE));
         textTitleScore.setFill(Color.GREY);
         //Texto para la puntuacion
-        Text textScore = new Text("0");
+        textScore = new Text("0");
         textScore.setFont(Font.font(TEXT_SIZE));
         textScore.setFill(Color.GRAY);
         //Texto de la etiqueta para la puntuacion maxima
@@ -91,24 +96,24 @@ public class Ponfxbueno extends Application {
         Text textHighScore = new Text("0");
         textHighScore.setFont(Font.font(TEXT_SIZE));
         textHighScore.setFill(Color.GRAY);
+        //Para reiniciar el juego
+        resetGame();
         //Añadir los textos a los layouts reservados para ellos
         paneCurrentScore.getChildren().add(textTitleScore);
         paneCurrentScore.getChildren().add(textScore);
         paneHighScore.getChildren().add(textTitleHighScore);
         paneHighScore.getChildren().add(textHighScore);
         //Creacion de la red del campo
-        for(int i=0; i<SCENE_TAM_Y; i+=30){
-        Line line = new Line(SCENE_TAM_X/2, i, SCENE_TAM_X/2, i+10);
-        line.setStroke(Color.BLUE);
-        line.setStrokeWidth(4);
-        root.getChildren().add(line);
-        }
+        drawNet(10, 4, 30);
         // Creacion del movimiento de la pala y bola
+        
         movimiento = new AnimationTimer() {
             //Actualizar la posicion de la pala
-                
+            //Detectar zona de colision de bola con la pala
+            
             @Override
             public void handle(long now) {
+                int collisionZone = getStickCollisionZone(bola, rectStick);
                 stickPosy += stickCurrentSpeed;
                 if(stickPosy < 0) {
                     //No sobrepasar el borde superior de la ventana
@@ -118,35 +123,36 @@ public class Ponfxbueno extends Application {
                         stickPosy = SCENE_TAM_Y - STICK_HEIGHT; 
                     }
                   }
-         
+            calculateBolaSpeed(collisionZone);
                   
                 //Mover rectangulo de la pala derecha a la posicion actual
                 rectStick.setY(stickPosy);
                 rectStick2.setY(stickPosy);
                 bola.setCenterX(bolaCenterX);
                 stickPosy += stickCurrentSpeed;
-                bolaCenterX+= velocidadbolax;
+                bolaCenterX+= velocidadBolax;
                 if (bolaCenterX >= SCENE_TAM_X){
                     if (score > highScore) {
                         highScore = score;
                         textHighScore.setText(String.valueOf(highScore));
                     }
+                    resetGame();
                     //Reiniciar partida
                     score = 0;
                     textScore.setText(String.valueOf(score));
                     bolaCenterX = 10;
-                    velocidadbolay = 3;
+                    velocidadBolay = 3;
                 }
                 if (bolaCenterX <= 0){
-                    velocidadbolax = 3;
+                    velocidadBolax = 3;
                 }
                 bola.setCenterY(bolaCenterY);
-                bolaCenterY+= velocidadbolay;
+                bolaCenterY+= velocidadBolay;
                 if (bolaCenterY >= SCENE_TAM_Y){
-                    velocidadbolay = -3;
+                    velocidadBolay = -3;
                 }
                 if (bolaCenterY <= 0){
-                    velocidadbolay = 3;
+                    velocidadBolay = 3;
                 }
                 
               
@@ -171,9 +177,9 @@ public class Ponfxbueno extends Application {
                 
                 boolean colisionVacia = shapeColision.getBoundsInLocal().isEmpty();
                
-                if(colisionVacia == false && velocidadbolax > 0){
+                if(colisionVacia == false && velocidadBolax > 0){
                     //Colision detectada. Mover bola hacia la izquierda
-                    velocidadbolax = -3;
+                    velocidadBolax = -3;
                     score++;
                     textScore.setText(String.valueOf(score));
                 }
@@ -204,8 +210,73 @@ public class Ponfxbueno extends Application {
         En estas dos lineas hemos declarado obtejos y sus atributos en una sola linea
         */
     }  
-               
-     public static void main(String[] args) {
+    
+    private void calculateBolaSpeed (int collisionZone) {
+        switch(collisionZone){
+            case 0:
+                //No hay colision
+                break;
+            case 1:
+                //Hay colision esquina superior
+                velocidadBolax = -3;
+                velocidadBolay = -6;
+                break;
+            case 2:
+                //Hay colision lado superior
+                velocidadBolax= -3;
+                velocidadBolay = -3;
+                break;
+            case 3:
+                //Hay colision lado inferior
+                velocidadBolax= -3;
+                velocidadBolay = 3;
+                break;
+            case 4:
+                //Hay colision esquina inferior
+                velocidadBolax= -3;
+                velocidadBolay = 6;
+                break;
+        }
+    }
+    
+    private int getStickCollisionZone(Circle bola, Rectangle stick){
+        if (Shape.intersect(bola, stick).getBoundsInLocal().isEmpty()){
+            return 0;
+        } else {
+            double offsetBolaStick = bola.getCenterY()- stick.getY();
+            if (offsetBolaStick < stick.getHeight() * 0.1){
+                return 1;
+            } else if (offsetBolaStick < stick.getHeight() / 2){
+                return 2;
+            } else if(offsetBolaStick >= stick.getHeight() / 2 &&
+                    offsetBolaStick < stick.getHeight() * 0.9 ){
+                return 3;
+            } else {
+                return 4;
+            }
+        }
+    }
+    
+    private void drawNet(int portionHeight, int portionWidth, int portionSpacing){
+        for(int i=0; i<SCENE_TAM_Y; i+=portionSpacing){
+            Line line = new Line(SCENE_TAM_X/2, i, SCENE_TAM_X/2, i+portionHeight);
+            line.setStroke(Color.BLUE);
+            line.setStrokeWidth(portionWidth);
+            root.getChildren().add(line);
+        }
+    }
+    
+    private void resetGame(){
+        score = 0;
+        textScore.setText(String.valueOf(score));
+        bolaCenterX = 10;
+        velocidadBolay = 3;
+        //Posicion inicial aleatoria para la bola en el eje y
+        Random random = new Random();
+        bolaCenterY = (short)random.nextInt(SCENE_TAM_Y);
+    }   
+    
+    public static void main(String[] args) {
         launch(args);
     }
     
